b7a5c2568805b7780b549de0d23e780eedf3e432
Maid <maidsivac@gmail.com>
2021-03-10 13:15:00 +0100

    Adding second task

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    // Some random line comment right here
+    /* This is part of multi-line comment 
+    This is part of multi-line comment 
+    This is part of multi-line comment 
+    This is part of multi-line comment 
+    This is part of multi-line comment  */
+    // Only change code below this line
+}
+
+console.log(main());
+
+
+module.exports = main;
